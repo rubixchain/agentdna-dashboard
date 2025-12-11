4c290d9baad200b1f20b55084bb0be656a3388ac
Arnab Ghose <arnab.ghose128@gmail.com>
2025-12-11 07:23:42 -0800

    added env var for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: 4001,      // change port
+    port: parseInt(process.env.DASHBOARD_SERVER_PORT, 10) || 2345,      // change port
     host: true,      // allow access from network (0.0.0.0)
-    strictPort: true // prevents auto-switching if port is busy
+    strictPort: true, // prevents auto-switching if port is busy
+    allowedHosts: [process.env.DASHBOARD_ALLOWED_HOSTS]
   }
 })
